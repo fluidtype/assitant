56c50902e39bd4a938eca49325e68c99cdb9803c
fluidtype <maicholz@icloud.com>
2025-09-14 14:20:34 +0200

    chore: export config defaults

--- src/services/booking/index.ts
+++ src/services/booking/index.ts
@@ -2,3 +2,4 @@ export * from './booking.service.js';
 export * from './availability.service.js';
 export * from './validation.service.js';
 export * from './opening-hours.util.js';
+export * from './config.defaults.js';
